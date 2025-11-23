40d3f609c395c7c80a540d8258fad04fe892dbce
Maulana <muammar676@gmail.com>
2025-11-23 12:25:04 +0700

    modified:   i3wm/i3/scripts/switch-workspace.sh

--- i3wm/i3/scripts/switch-workspace.sh
+++ i3wm/i3/scripts/switch-workspace.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 # Ambil daftar workspace
-workspaces=$(i3-msg -t get_workspaces | jq -r '.[].name')
-
+workspaces=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused == false) | .name')
 # Tampilkan workspace di rofi
-selected=$(echo "$workspaces" | rofi -dmenu -p "Workspace")
+selected=$(echo "$workspaces" | rofi -dmenu -lines 10 -width 40 -p "Workspace")
+
+# Jika tidak ada workspace non-aktif, keluar
+if [ -z "$workspaces" ]; then
+    notify-send "Tidak ada workspace non-aktif"
+    exit 0
+fi
 
 # Pindah ke workspace yang dipilih
 if [ -n "$selected" ]; then
     i3-msg workspace "$selected"
 fi
+
