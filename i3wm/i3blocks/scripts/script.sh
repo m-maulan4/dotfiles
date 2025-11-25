e8e2461efd337b2e0fe730f79f63ae16c121045a
Maulana <muammar676@gmail.com>
2025-11-25 11:21:09 +0700

    modified:   i3wm/i3blocks/config 	modified:   i3wm/i3blocks/scripts/script.sh

--- i3wm/i3blocks/scripts/script.sh
+++ i3wm/i3blocks/scripts/script.sh
@@ -2,8 +2,9 @@
 title(){
     CheckIdWorkspace=$(xprop -root _NET_ACTIVE_WINDOW | awk '{print $5}')
     Title=$(xprop -id $CheckIdWorkspace 'WM_CLASS' | awk '{gsub(/"/,"",$4); gsub(/-/," ",$4); print $4}' )
-    Color=$(head /dev/urandom | tr -dc 0-9A-F | head -c 6)
-    echo -e "$Title\n\n#$Color"
+    Color=$(head /dev/urandom | tr -dc 8-9A-F | head -c 6)
+    echo "<span color='#$Color'>$Title</span>"
+
 }
 wifi(){
     WiFi=$(nmcli -t -f NAME connection show --active | grep -v lo)
@@ -13,7 +14,7 @@ wifi(){
     else
         Color="#00FF00"
     fi
-    echo -e "$WiFi\n\n$Color"
+    echo "<span color='$Color'> $WiFi</span>"
 }
 bt(){
     device=$(bluetoothctl devices Connected | cut -d' ' -f4-); [ -z "$device" ] 
@@ -23,7 +24,7 @@ bt(){
     else
         Color="#00FF00"
     fi
-    echo -e "$device\n\n$Color"
+    echo "<span color='$Color'> $device</span>"
 }
 vol(){
     CheckIdVol=$(wpctl status | grep "Built-in Audio Analog Stereo" | awk '{print $3}'| head -n1 |sed 's/\.//')
@@ -35,7 +36,7 @@ vol(){
     else if (vol >= 25)     { print "#669900" }
     else { print "#336600" }
     }')
-    echo -e "$VOL\n\n$Color"
+    echo "<span color='$Color'> $VOL%</span>"
 }
 cpu(){
     CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print $2+$4+$6+$10+$12+$14}')
@@ -45,14 +46,14 @@ cpu(){
     else
         Color="#00FF00"
     fi
-    echo -e "$CPU%\n\n$Color"
+    echo "<span color='$Color'> $INT%</span>"
 }
 ram(){
     TRam=$(free -h | head -n 2 | tail -n 1 | awk '{gsub(/Gi/,"");print $2}')
     TUsage=$(free -h | head -n 2 | tail -n 1 | awk '{gsub(/Gi/,"");print $3}')
-    RAM=$(awk "BEGIN {printf \"%.0f\", $TUsage/$TRam*100}")
-    Color=$(awk -v ram="$RAM" 'BEGIN {if(ram>=10) print "#FF0000"; else print "#00FF00"}')
-    echo -e "$RAM%\n\n$Color"
+    RAM=$(awk "BEGIN {printf int($TUsage/$TRam*100)}")
+    Color=$(awk -v ram="$RAM" 'BEGIN {if(ram>=70) print "#FF0000"; else print "#00FF00"}')
+    echo "<span color='$Color'> $RAM%</span>"
 }
 bat(){
     VALUE=$(acpi -b | awk -F', ' '{print $2}' | tr -d ' %')
@@ -67,32 +68,11 @@ bat(){
         STATUS="[IDLE]"
         Color="#0096FF"
     fi
-    echo -e "$VALUE% $STATUS\n\n$Color"
+    echo "<span color='$Color'> $VALUE% $STATUS</span>"
 }
-# printah terminal
-case "$1" in
-    title)
-        title  
-        ;;
-    wifi)
-        wifi  
-        ;;
-    bt)
-        bt  
-        ;;
-    vol)
-        vol  
-        ;;
-    cpu)
-        cpu  
-        ;;
-    ram)
-        ram  
-        ;;
-    bat)
-        bat  
-        ;;
-    *)
-        exit 0
-        ;;
-esac
+dt(){
+    DATE=$(date '+%d-%m-%Y %H:%M')
+    echo $DATE
+}
+
+echo "$(title) | $(wifi) | $(bt) | $(vol) | $(cpu) | $(ram) | $(bat) | $(dt)"
