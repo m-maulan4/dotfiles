a64030e48f50f6f5539c246bfc0b258f4240dae0
Maulana <muammar676@gmail.com>
2025-11-28 14:37:16 +0700

    modified:   .config/i3/config 	modified:   .config/i3/scripts/i3bar_custom.sh 	modified:   .config/picom/picom.conf 	new file:   .config/rofi/config.rasi

--- .config/i3/scripts/i3bar_custom.sh
+++ .config/i3/scripts/i3bar_custom.sh
@@ -3,7 +3,7 @@
 title(){
     CheckIdWorkspace=$(xprop -root _NET_ACTIVE_WINDOW | awk '{print $5}')
     Title=$(xprop -id $CheckIdWorkspace 'WM_CLASS' | awk '{gsub(/"/,"",$4); gsub(/-/," ",$4); print $4}' )
-    echo "{\"color\":\"#FFFFFF\",\"full_text\":\"$Title\"}"
+    echo "{\"color\":\"#FFFFFF\",\"full_text\":\"$Title\"},"
 
 }
 wifi(){
@@ -14,17 +14,13 @@ wifi(){
     else
         Color="#00FF00"
     fi
-    echo "{\"color\":\"$Color\",\"full_text\":\" $WiFi\"}"
+    echo "{\"color\":\"$Color\",\"full_text\":\" $WiFi\"},"
 }
 bt(){
     device=$(bluetoothctl devices Connected | cut -d' ' -f4-); [ -z "$device" ] 
-    if [ -z "$device" ]; then
-        device="DOWN"
-        Color="#FF0000"
-    else
-        Color="#00FF00"
+    if [ -n "$device" ]; then
+	echo "{\"color\":\"#00FF00\",\"full_text\":\" $device\"},"
     fi
-    echo "{\"color\":\"$Color\",\"full_text\":\" $device\"}"
 }
 vol(){
     CheckIdVol=$(wpctl status | grep "Built-in Audio Analog Stereo" | awk '{print $3}'| head -n1 |sed 's/\.//')
@@ -36,7 +32,7 @@ vol(){
     else if (vol >= 25)     { print "#669900" }
     else { print "#336600" }
     }')
-    echo "{\"color\":\"$Color\",\"full_text\":\" $VOL%\"}"
+    echo "{\"color\":\"$Color\",\"full_text\":\" $VOL%\"},"
 }
 cpu(){
     CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print $2+$4+$6+$10+$12+$14}')
@@ -46,14 +42,14 @@ cpu(){
     else
         Color="#00FF00"
     fi
-    echo "{\"color\":\"$Color\",\"full_text\":\" $INT%\"}"
+    echo "{\"color\":\"$Color\",\"full_text\":\" $INT%\"},"
 }
 ram(){
     TRam=$(free -w | head -n 2 | tail -n 1 | awk '{print $2}' | sed 's/..$//')
     TUsage=$(free -w | head -n 2 | tail -n 1 | awk '{print $3}' | sed 's/..$//')
     RAM=$(awk "BEGIN {printf int($TUsage/$TRam*100)}")
     Color=$(awk -v ram="$RAM" 'BEGIN {if(ram>=70) print "#FF0000"; else print "#00FF00"}')
-    echo "{\"color\":\"$Color\",\"full_text\":\" $RAM%\"}"
+    echo "{\"color\":\"$Color\",\"full_text\":\" $RAM%\"},"
 }
 bat(){
     VALUE=$(acpi -b | awk -F', ' '{print $2}' | tr -d ' %')
@@ -68,17 +64,18 @@ bat(){
         STATUS="[IDLE]"
         Color="#0096FF"
     fi
-    echo "{\"color\":\"$Color\",\"full_text\":\" $VALUE% $STATUS\"}"
+    echo "{\"color\":\"$Color\",\"full_text\":\" $VALUE% $STATUS\"},"
 }
 dt(){
-    DATE=$(date '+%d-%m-%Y')
-    TIME=$(date '+%H:%M:$S')
-    echo "{\"color\":\"#FFFFFF\",\"full_text\":\" $DATE  $TIME\"}"
+    DATE=$(date '+%d/%m/%Y')
+    TIME=$(date '+%H:%M:%S')
+    echo "{\"color\":\"#FFFFFF\",\"full_text\":\"$DATE $TIME\"}"
 }
 echo '{"version":1}'
 echo '['
 
 while true; do
-    echo "[$(title),$(wifi),$(bt),$(vol),$(cpu),$(ram),$(bat),$(dt)],"
+    echo "[$(title)$(wifi)$(bt)$(vol)$(cpu)$(ram)$(bat)$(dt)],"
     sleep 1
 done
+
